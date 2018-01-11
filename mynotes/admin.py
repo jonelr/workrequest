a5445704d32a27c278ab083cf2e9d77f089c9bd8
Jonel Rienton <jrienton@littelfuse.com>
2018-01-11 14:43:02 -0600

    Add account in Training class

--- mynotes/admin.py
+++ mynotes/admin.py
@@ -6,4 +6,18 @@ from .models import Training
 # Register your models here.
 @admin.register(Training)
 class TrainingAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'date_taken', 'account')
+    search_fields = ('title',)
+    exclude = ('account',)
+
+    def save_model(self, request, obj, form, change):
+        print(request.user)
+        if not obj.account:
+            obj.account = request.user
+        super(TrainingAdmin, self).save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super(TrainingAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(account=request.user)
