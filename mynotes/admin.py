36740f559e1ee8760b9fadc6c6c260d990c8a616
Jonel Rienton <jrienton@littelfuse.com>
2018-01-11 14:28:54 -0600

    Add training to mynotes app

--- mynotes/admin.py
+++ mynotes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Training
+
+
 # Register your models here.
+@admin.register(Training)
+class TrainingAdmin(admin.ModelAdmin):
+    pass
--- mynotes/models.py
+++ mynotes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Training(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+    date_taken = models.DateField(default=datetime.now)
+
+    def __str__(self):
+        return self.title
